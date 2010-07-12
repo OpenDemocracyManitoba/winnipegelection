2e8f0283e9d5b9671247c931ab9e96b938d5b72c
Wally Glutton <stungeye@gmail.com>
2010-07-12 00:15:35 +0000

    About to add ward types.

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -2,4 +2,9 @@ class InfoController < ApplicationController
   def wards
     @wards = Ward.all(:include => :candidates)
   end
+  def councillors
+    @wards = Ward.all(:include => :candidates)
+    @title = 'City Councillors'
+    render :partial => 'ward', :layout => 'application'
+  end
 end
