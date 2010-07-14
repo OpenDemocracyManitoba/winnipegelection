f4ee3fc17aee3efd4076df8d7b5709eda7a1bf71
Wally Glutton <stungeye@gmail.com>
2010-07-14 13:10:56 +0000

    Tweaking some of the info partials

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -1,15 +1,17 @@
 class InfoController < ApplicationController
-  def council
-    @wards = Ward.all(:include => :candidates, :conditions => "ward_type = 'Civic'")
-    @title = 'Winnipeg City Councillor Candidates'
-  end
   def mayor
     @ward = Ward.first(:include => :candidates, :conditions => "ward_type = 'Mayoral'")
     @title = 'Winnipeg Mayoral Candidates'
   end
+  def council
+    @wards = Ward.all(:include => :candidates, :conditions => "ward_type = 'Civic'")
+    @title = 'Winnipeg City Councillor Candidates'
+    @wards_title = 'All Electoral Wards'
+  end
   def school_trustee
     @wards = Ward.all(:include => :candidates, :conditions => "ward_type = 'School Division'")
     @title = 'Winnipeg School Trustee Candidates'
+    @wards_title = 'All School Divisions'
   end
   def ward
     @ward_name = params[:ward_name].sub(/^St/,'St.').gsub('_',' ')
