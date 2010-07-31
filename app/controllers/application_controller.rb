a893e0adfe2b599fd1e5a7b5c95333f856f6b171
Wally Glutton <stungeye@gmail.com>
2010-07-31 19:41:41 +0000

    Added a tiny app config library.

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,19 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+  protect_from_forgery # See ActionController::RequestForgeryProtection for details
+
+  # Scrub sensitive parameters from your log
+  # filter_parameter_logging :password
+  
+protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == AppConfig.admin_user && password == AppConfig.admin_password
+    end
+  end
+  
+end
