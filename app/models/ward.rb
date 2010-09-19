ffefc9afb2606ca3cce049295f79fca7d976e534
Wally Glutton <stungeye@gmail.com>
2010-09-19 18:11:23 +0000

    Added images of ward maps to each ward. Added new menu options.

--- app/models/ward.rb
+++ app/models/ward.rb
@@ -6,9 +6,9 @@ class Ward < ActiveRecord::Base
   validates_presence_of :name, :ward_type
   validates_inclusion_of :ward_type, :in => WARD_TYPES, :message => "must be: #{WARD_TYPES.join(', ')}"
   
-  attr_accessible :name, :pdf_map_url, :ward_type, :image, :stats, :past_votes
+  attr_accessible :name, :pdf_map_url, :ward_type, :image, :stats, :past_votes, :delete_image
   
-  has_attached_file :image, :styles => { :large => "700x700>", :medium => "600x600>", :small => "500x500>", :thumb => "300x300>" },
+  has_attached_file :image, :styles => { :large => "300x300>", :medium => "240x240>", :small => "120x120>", :tiny => "60x60>" },
                     :url  => "/uploads/ward_image/:id/:style/:basename.:extension",
                     :path => ":rails_root/public/uploads/ward_image/:id/:style/:basename.:extension"
   
@@ -26,5 +26,14 @@ class Ward < ActiveRecord::Base
   
   def self.url_to_ward(url)
     url.sub(/^St/,'St.').gsub('_',' ')
+  end
+  
+  # Virtual Attribute for image deletion.
+  def delete_image=(value)
+    self.image = nil if (!value.to_i.zero?)
+  end
+  
+  def delete_image
+    false
   end
 end
