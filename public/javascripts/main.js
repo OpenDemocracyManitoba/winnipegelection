5c8551f8c3a22bef48471182dac01316fca9c3df
Wally Glutton <stungeye@gmail.com>
2010-09-25 23:18:08 +0000

    Added Twitter widget for wpgvotes hashtag to Latest News.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -23,15 +23,8 @@ $(document).ready(function() {
     
     // Users thought these login / register links were for our site.
     setTimeout(function() {
-        $("div.urtakw-footer-left").fadeOut();
-        $("div.urtakw-footer-right").mouseenter(function(){
-            $("div.urtakw-footer-left").fadeIn();
-        });
-        $("div.urtakw-footer").mouseleave(function(){
-            $("div.urtakw-footer-left").fadeOut();
-        });
-        $("div.urtakw-footer-right a").attr("title","Polling provided by Urkat.")
-    }, 4500);
+        $("div.twtr-ft").fadeOut();
+    }, 4000);
 
     $('div.candidates').shuffle();
 })
