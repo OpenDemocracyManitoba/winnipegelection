6da362ea9b22626b037e401aff159dee19462989
Wally Glutton <stungeye@gmail.com>
2011-12-22 23:01:46 -0600

    Removed all the vendor/gems. Upgraded rails version to 2.3.14 and added version info to all the gems listed in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.14' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -19,12 +19,12 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "haml"
-  config.gem "rdiscount"
-  config.gem "feed-normalizer"
-  config.gem 'nokogiri'
-  config.gem 'paged_scopes'
-  #config.gem "newrelic_rpm"
+  #
+  config.gem 'haml', :version => '3.0.25'
+  config.gem "rdiscount", :version => '1.6.8'
+  config.gem "feed-normalizer", :version => '1.5.2'
+  config.gem 'nokogiri', :version => '1.4.4'
+  config.gem 'paged_scopes', :version => '0.1.1'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
