2a9e6b443911a9630e6551893a2fd37ae057052b
Kyle Geske <stuneye@gmail.com>
2014-04-15 15:18:48 -0400

    Added a Rubocop config. Minor tweaks here and there to pass Rubocop tests.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,7 @@ class PagesController < ApplicationController
   def index
     @pages = Page.all
   end
+
   def show
     @page = Page.find_page(params)
   end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,29 +1,32 @@
 class Page < ActiveRecord::Base
   include Rails.application.routes.url_helpers
-  
+
   validates :permalink, presence: true
-  
-  # Ensures that the permalink is URL encoded. 
+
+  # Ensures that the permalink is URL encoded.
   def permalink_param
     permalink.parameterize
   end
-  
+
   # Builds a friendly URL that includes the permalink
   # Used like this: link_to page.title, page.friendly_path
   def friendly_path
     page_path(id: id, permalink: permalink_param)
   end
-  
+
   # Synonym for title for ActiveAdmin which assumes a name property.
   def name
-    self.title
+    title
   end
-  
+
   # Finds page by id, but throws an exception if the permalink slug from the
   # URL doesn't match the page permalink.
   def self.find_page(params)
     page = find(params[:id])
-    raise ActiveRecord::RecordNotFound  if page.permalink_param != params[:permalink]
-    page
+    if page.permalink_param != params[:permalink]
+      fail ActiveRecord::RecordNotFound
+    else
+      page
+    end
   end
 end
