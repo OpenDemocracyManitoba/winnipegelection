b1fa1e6b97328f4dc8c837991ed9727440b60474
Kyle Geske <stungeye@gmail.com>
2014-04-29 12:12:06 -0400

    Added active admin permitted params.

--- app/admin/candidacy.rb
+++ app/admin/candidacy.rb
@@ -1,17 +1,5 @@
 ActiveAdmin.register Candidacy do
 
-  
-  # See permitted parameters documentation:
-  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #  permitted = [:permitted, :attributes]
-  #  permitted << :other if resource.something?
-  #  permitted
-  # end
+  permit_params :questionnaire, :end_date, :is_incumbent, :is_winner, :person_id, :electoral_race_id
   
 end
