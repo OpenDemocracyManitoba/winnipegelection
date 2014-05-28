917cddae9491a29aa1285a494cb7a62bd7fa5e88
Kyle Geske <stungeye@gmail.com>
2014-05-28 14:15:28 -0400

    Needed to update active admin params permitted so that region seats and electoral_races seats_to_fill could be updated.

--- app/admin/electoral_race.rb
+++ app/admin/electoral_race.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register ElectoralRace do
 
-  permit_params :region_id, :election_id, :polygon
+  permit_params :region_id, :election_id, :polygon, :seats_to_fill
   
   index do
     selectable_column
@@ -8,6 +8,7 @@ ActiveAdmin.register ElectoralRace do
     column :name, sortable: true do |electoral_race|
       link_to electoral_race.name, edit_admin_electoral_race_path(electoral_race)
     end
+    column :seats_to_fill
     column :polygon do |electoral_race|
       truncate electoral_race.polygon
     end
--- app/admin/region.rb
+++ app/admin/region.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Region do
 
-  permit_params :name, :start_date, :end_date, :region_type_id, :region_id
+  permit_params :name, :start_date, :end_date, :region_type_id, :region_id, :seats
   
 end
