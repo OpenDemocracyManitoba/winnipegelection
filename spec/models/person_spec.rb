415716e1e23f9dd7b6162a265b2d84d6e041fba0
Kyle Geske <stungeye@gmail.com>
2014-06-04 01:51:58 -0400

    Separated some of the duplicated friendly url tests from page and person specs into a shared example.

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -12,20 +12,14 @@ describe Person do
   end
 
   context 'when class is augmented with FriendlyURL concern' do
+    it_behaves_like FriendlyURL do
+      let(:object_of_described_class) { FactoryGirl.create(:person, name: 'Test Slug') }
+    end
+
     let(:person) { FactoryGirl.create(:person, name: 'Walter Glutton') }
 
     it 'returns a friendly url path' do
       expect(person.friendly_path).to match %r"^/people/\d+/walter-glutton$"
-    end
-
-    it 'can find a person by id and slug' do
-      found_person = Person.friendly_find(id: person.id, slug: 'walter-glutton')
-      expect(found_person).to be_a Person
-    end
-
-    it 'raises an error when finding a person when slug is incorrect or missing' do
-      expect { Person.friendly_find(id: person.id, slug: 'wrong-slug') }.to raise_error
-      expect { Person.friendly_find(id: person.id) }.to raise_error
     end
   end
 end
