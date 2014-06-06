ab3b60fe8eb4a3d9bd4dd8b38293e8b1be1abe53
Kyle Geske <stungeye@gmail.com>
2014-06-06 16:45:46 -0400

    No more days until election date math in the controller.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
     @active_election = Election.active_election
-    @days_until_election = @active_election.election_date - Date.today
   end
 end
