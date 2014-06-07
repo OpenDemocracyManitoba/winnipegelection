bd3d187ac64e10cdf0566907d059a6162c4f734d
Kyle Geske <stungeye@gmail.com>
2014-06-07 10:36:21 -0400

    The active election is always available via an helper method defined on application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  def active_election
+    @active_election ||= Election.active_election
+  end
+  helper_method :active_election
 end
