7d5614b16c42402176fd331b366cbbfda3d0cc0e
Kyle Geske <stungeye@gmail.com>
2014-06-13 00:00:11 -0400

    Fix for the 2010-era candidate url people redirect.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -6,7 +6,7 @@ class PeopleController < ApplicationController
   def redirect_show
     person = Person.find_by(name: params[:name_slug].gsub('_', ' '))
     if person
-      redirect_to person_url(id: person.id, slug: person.slug), status: 301
+      redirect_to person.friendly_path, status: 301
     else
       fail ActiveRecord::RecordNotFound
     end
