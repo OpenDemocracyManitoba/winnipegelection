b7b26afb51778d7e919edbe00f7253594181030e
Kyle Geske <stungeye@gmail.com>
2014-06-13 00:10:02 -0400

    Added a feature spec for old-style candidate url redirects.

---
+++ spec/features/user_follows_old_style_url_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+feature 'User follows a 2010-era candidate URL' do
+  before(:each) do
+    visit '/candidate/Darth_Vadar'
+  end
+
+  scenario 'the page is successfully rendered' do
+    expect(page.status_code).to be(200)
+  end
+
+  scenario 'the user is redirected to the new-style friendly path' do
+    darth = people(:darth_vadar)
+    expect(current_path).to eq(darth.friendly_path)
+  end
+
+  scenario 'the user has arrived at the correct page' do
+    expect(page).to have_content('Darth Vadar')
+  end
+end
