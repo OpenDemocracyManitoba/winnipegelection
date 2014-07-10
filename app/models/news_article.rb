42db5b655e009b62347036c050832bb4e0873f50
Kyle Geske <stungeye@gmail.com>
2014-07-10 16:09:11 -0400

    Fake enum on NewsArticle moderation property via inclusion validation.

--- app/models/news_article.rb
+++ app/models/news_article.rb
@@ -1,9 +1,12 @@
 class NewsArticle < ActiveRecord::Base
+  MODERATION_OPTIONS = %w(new approved rejected)
+
   belongs_to :news_source, inverse_of: :news_articles
   has_many :news_mentions, inverse_of: :news_article, dependent: :destroy
   has_many :people, through: :news_mentions
 
   validates :news_source, presence: true
+  validates :moderation, inclusion: { in: MODERATION_OPTIONS }
 
   def pretty_date
     publication_date.blank? ? '' : publication_date.strftime('%A, %d %B %Y')
