b89c5fc7344d6a529d0de55c938c5060db7d2114
Jeffrey Fulton <jeffrey.fulton@me.com>
2014-07-24 11:44:01 -0500

    Order sub-nav links by name

--- app/models/navigation_heading.rb
+++ app/models/navigation_heading.rb
@@ -1,6 +1,6 @@
 class NavigationHeading < ActiveRecord::Base
-	has_many :pages
-	has_many :electoral_races
+	has_many :pages, -> { order 'title' }
+	has_many :electoral_races, -> { includes(:region).order('regions.name') }
 
 	def items
 		pages + electoral_races
