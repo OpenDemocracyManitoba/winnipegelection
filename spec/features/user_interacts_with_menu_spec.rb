b9eaa184fe7fe404f0020bf32c402fb5e56e456d
Kyle Geske <stungeye@gmail.com>
2014-08-09 08:52:42 -0500

    Refactored the navigation menu feature spec: more nested contexts, one expect per scenario, DRY CSS selectors.

--- spec/features/user_interacts_with_menu_spec.rb
+++ spec/features/user_interacts_with_menu_spec.rb
@@ -1,77 +1,100 @@
 require 'spec_helper'
 
 feature 'User interacts with navigation menu' do
-  before(:each) do
-    visit '/'
-  end
-
   context 'without existing NavigationHeading records' do
-    before(:each) do
+    scenario 'they see no navigation menu' do
       NavigationHeading.destroy_all
       visit '/'
-    end
-
-    scenario 'they see no navigation menu' do
       expect(page).to_not have_css('nav.menu-holder')
     end
   end
 
   context 'with existing NavigationHeading records' do
-    given(:home_nav_heading) { navigation_headings(:home) }
-    given(:candidates_nav_heading) { navigation_headings(:candidates) }
-    given(:about_nav_heading) { navigation_headings(:about) }
+    before(:each) do
+      visit '/'
+    end
 
-    given(:about_page) { pages(:about) }
-
-    given(:city_of_winnipeg_2014_election_race) {
-      electoral_races(:city_of_winnipeg_2014_election_race)
+    given(:top_level_nav_link_selector) { '.menu > ul.primary > li > a' }
+    given(:sub_nav_link_selector) {
+      '.menu > ul.primary > li.has-sub > ul.secondary > li > a'
     }
-
-    given(:st_vital_2014_election_race) {
-      electoral_races(:st_vital_2014_election_race)
-    }
-
-    given(:mayoral_sub_heading) { navigation_headings(:mayoral_candidates) }
 
     scenario 'they see navigation menu' do
       expect(page).to have_css('nav.menu-holder')
     end
 
-    scenario 'they see a top-level navigation link for each NavigationHeading' do
-      within('nav.menu-holder') do
-        selector = '.menu > ul.primary > li > a'
-        expect(page).to have_css(selector, text: home_nav_heading.name)
-        expect(page).to have_css(selector, text: candidates_nav_heading.name)
-        expect(page).to have_css(selector, text: about_nav_heading.name)
+    context 'with a top-level NavigationHeading record with a url' do
+      given(:home_nav_heading) { navigation_headings(:home) }
+
+      scenario 'they can navigate to a URL by way of the navigation link' do
+        within('nav.menu-holder') { click_link('Home') }
+        expect(page.status_code).to be(200)
+      end
+
+      scenario 'they see the top-level navigation link in the menu' do
+        within('nav.menu-holder') do
+          expect(page).to have_css(top_level_nav_link_selector,
+                                   text: home_nav_heading.name)
+        end
       end
     end
 
-    scenario 'they see list of all associated items in subnavs' do
-      within('nav.menu-holder') do
-        selector = '.menu > ul.primary > li.has-sub > ul.secondary > li > a'
-        expect(page).to have_css(selector, text: about_page.title)
-        expect(page).to have_css(selector,
-                                 text: city_of_winnipeg_2014_election_race.name)
-        expect(page).to have_css(selector,
-                                 text: st_vital_2014_election_race.name)
-        expect(page).to have_css(selector, text: mayoral_sub_heading.name)
+    context 'with a top-level NavigationHeading record without a url' do
+      given(:about_nav_heading) { navigation_headings(:about) }
+
+      scenario 'they see the top-level navigation link in the menu' do
+        within('nav.menu-holder') do
+          expect(page).to have_css(top_level_nav_link_selector,
+                                   text: about_nav_heading.name)
+        end
       end
     end
 
-    scenario 'they can navigate to Home page by clicking the navigation link "Home"' do
-      within('nav.menu-holder') { click_link('Home') }
-      expect(page.status_code).to be(200)
+    context 'with a CMS page as a sub-navigation record' do
+      given(:about_page) { pages(:about) }
+
+      scenario 'they see the CMS page link as a sub-navigation link' do
+        within('nav.menu-holder') do
+          expect(page).to have_css(sub_nav_link_selector, text: about_page.title)
+        end
+      end
+
+      scenario 'they can navigate to a CMS page by clicking a sub-navigation link' do
+        within('nav.menu-holder') { click_link('About Us') }
+        expect(page).to have_css('#main h1', text: about_page.title)
+      end
     end
 
-    scenario ' they can navigate to a url via a sub-heading' do
-      within('nav.menu-holder') { click_link('Council') }
-      expect(page.status_code).to be(200)
+    context 'with a ElectoralRace as a sub-navigation record' do
+      given(:electoral_race) {
+        electoral_races(:city_of_winnipeg_2014_election_race)
+      }
+
+      scenario 'they see the ElectoralRace as a sub-navigation link' do
+        within('nav.menu-holder') do
+          expect(page).to have_css(sub_nav_link_selector, text: electoral_race.name)
+        end
+      end
+
+      scenario 'they can navigate to an Electoral by clicking a sub-navigation link' do
+        within('nav.menu-holder') { click_link(electoral_race.name) }
+        expect(page).to have_css('#main h2', text: electoral_race.name)
+      end
     end
 
-    scenario 'they can navigate to a CMS page by clicking a sub-navigation link' do
-      within('nav.menu-holder') { click_link('About Us') }
-      expect(page.status_code).to be(200)
-      expect(page).to have_css('#main h1', text: about_page.title)
+    context 'with a NavigationHeading as a sub-navigation record' do
+      given(:mayoral_sub_heading) { navigation_headings(:mayoral_candidates) }
+
+      scenario 'they see the NavigationHeading as a sub-navigation link' do
+        within('nav.menu-holder') do
+          expect(page).to have_css(sub_nav_link_selector, text: mayoral_sub_heading.name)
+        end
+      end
+
+      scenario ' they can navigate to a url via the sub-navigation link' do
+        within('nav.menu-holder') { click_link('Council') }
+        expect(page.status_code).to be(200)
+      end
     end
   end
 end
