d422e4b08dc82f245611dc97c818320d0748f63b
Kyle Geske <stungeye@gmail.com>
2014-08-27 10:44:02 -0400

    A person's show page now links to their most recent electoral ward.

--- spec/features/user_visits_person_page_spec.rb
+++ spec/features/user_visits_person_page_spec.rb
@@ -1,14 +1,13 @@
 require 'spec_helper'
 
 feature 'User visits a Person page' do
-
-  context 'Person in active election with questionnaire' do
-    before(:each) do
-      visit darth_vadar.friendly_path
-    end
-
+  context 'Any Person' do
     given(:darth_vadar) do
       people(:darth_vadar)
+    end
+
+    before(:each) do
+      visit darth_vadar.friendly_path
     end
 
     scenario 'the page successfully loads' do
@@ -22,13 +21,71 @@ feature 'User visits a Person page' do
     scenario "they see the person's name in the browser title" do
       expect(page).to have_title(darth_vadar.name)
     end
+  end
 
-    scenario 'they see questionnaire if a person has replied' do
-      expect(page).to have_css('h2', text: 'Questionnaire Response')
+  context 'Person is part of the active election' do
+    given(:darth_vadar) do
+      people(:darth_vadar)
+    end
+
+    before(:each) do
+      visit darth_vadar.friendly_path
     end
 
     scenario 'they do not see a warning if the person is in active election' do
       expect(page).to_not have_content('information on this page was collected')
+    end
+
+    scenario 'they see the name of the active electoral race' do
+      expect(page).to have_content('2014 City of Winnipeg Mayoral race')
+    end
+
+    scenario 'they can navigate to the active electoral race' do
+      click_link('2014 City of Winnipeg Mayoral race')
+      expect(page).to have_title('Mayoral: City of Winnipeg - 2014')
+    end
+  end
+
+  context 'Person not running in active election' do
+    given(:tyrion_lannister) do
+      people(:tyrion_lannister)
+    end
+
+    scenario 'they see an warning if the person is not in active election' do
+      visit tyrion_lannister.friendly_path
+      expect(page).to have_content('information on this page was collected')
+    end
+  end
+
+  context 'Person with questionnaire' do
+    given(:darth_vadar) do
+      people(:darth_vadar)
+    end
+
+    scenario 'they see questionnaire if a person has replied' do
+      visit darth_vadar.friendly_path
+      expect(page).to have_css('h2', text: 'Questionnaire Response')
+    end
+  end
+
+  context 'Person without questionnaire' do
+    given(:luke_skywalker) do
+      people(:luke_skywalker)
+    end
+
+    scenario 'they do not see questionnaire if a person has not replied' do
+      visit luke_skywalker.friendly_path
+      expect(page).to_not have_css('h2', text: 'Questionnaire Response')
+    end
+  end
+
+  context 'Person with related NewsMentions' do
+    given(:darth_vadar) do
+      people(:darth_vadar)
+    end
+
+    before(:each) do
+      visit darth_vadar.friendly_path
     end
 
     scenario 'they see the correct related articles link' do
@@ -45,33 +102,14 @@ feature 'User visits a Person page' do
     end
   end
 
-  context 'Person in active election without questionnaire' do
-    given(:luke_skywalker) do
-      people(:luke_skywalker)
-    end
-
-    scenario 'they do not see questionnaire if a person has not replied' do
-      visit luke_skywalker.friendly_path
-      expect(page).to_not have_css('h2', text: 'Questionnaire Response')
-    end
-  end
-
-  context 'Person not running in active election with no news mentions' do
-    before(:each) do
-      visit tyrion_lannister.friendly_path
-    end
-
+  context 'Person with no NewsMentions' do
     given(:tyrion_lannister) do
       people(:tyrion_lannister)
     end
 
-    scenario 'they see an warning if the person is not in active election' do
-      expect(page).to have_content('information on this page was collected')
-    end
-
     scenario 'they see a message about no news articles' do
+      visit tyrion_lannister.friendly_path
       expect(page).to have_content('No related news articles found.')
     end
   end
-
 end
