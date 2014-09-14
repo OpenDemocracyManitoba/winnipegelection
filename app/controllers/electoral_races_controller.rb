c32d60b89df77fdc9c1d5c3a07688450a777305e
Kyle Geske <stungeye@gmail.com>
2014-09-14 14:39:09 -0400

    Added JSON API output to council and school_trustee electoral race collections.

--- app/controllers/electoral_races_controller.rb
+++ app/controllers/electoral_races_controller.rb
@@ -1,10 +1,22 @@
 class ElectoralRacesController < ApplicationController
   def council
     @electoral_races = active_council_races_order_by_region_name
+    respond_to do |format|
+      format.html
+      format.json {
+        render json: @electoral_races, include: [:region, candidacies: {include: :person}]
+      }
+    end
   end
 
   def school_trustee
     @electoral_races = active_school_trustee_races_order_by_region_name
+    respond_to do |format|
+      format.html
+      format.json {
+        render json: @electoral_races, include: [:region, candidacies: {include: :person}]
+      }
+    end
   end
 
   def show
@@ -15,6 +27,6 @@ class ElectoralRacesController < ApplicationController
   def show_json
     @electoral_race = ElectoralRace.includes(candidacies: :person )
                                    .find(params[:id])
-    render json: @electoral_race, include: [:region, candidacies: {include: :person}]  
+    render json: @electoral_race, include: [:region, candidacies: {include: :person}]
   end
 end
