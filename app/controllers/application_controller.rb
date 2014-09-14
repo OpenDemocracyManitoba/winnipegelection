9b8f8c5b19be18fd8b376709bdc2587f0d0a5c6a
Jeffrey Fulton <jeffrey.fulton@me.com>
2014-09-14 16:12:38 -0500

    Add logic to determine each digit of Voter Countdown flip-counter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,4 +36,20 @@ class ApplicationController < ActionController::Base
     @nav_headings ||= NavigationHeading.parent_navigation_headings
   end
   helper_method :nav_headings
+
+  def days_until_active_election_tens_place
+    active_election.days_until_election.to_s.rjust(2, '0')[-2]
+  end
+  helper_method :days_until_active_election_tens_place
+
+  def days_until_active_election_ones_place
+    active_election.days_until_election.to_s.rjust(2, '0')[-1]
+  end
+  helper_method :days_until_active_election_ones_place
+
+  def active_election_date_in_english
+    election_date = active_election.election_date
+    election_date.strftime("%A, %B #{election_date.day.ordinalize} %Y")
+  end
+  helper_method :active_election_date_in_english
 end
