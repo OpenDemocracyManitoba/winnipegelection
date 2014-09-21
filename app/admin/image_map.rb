6ba55a5e89477fb82852c7909437a5402431ac72
Kyle Geske <stungeye@gmail.com>
2014-09-21 09:34:24 -0400

    Active Admin tweaks for image map and image map polygon.

--- app/admin/image_map.rb
+++ app/admin/image_map.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register ImageMap do
   permit_params :name, :image
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :image do |image_map|
+      image_tag image_map.image.small.url
+    end
+    column :created_at
+    column :updated_at
+    default_actions
+  end
 end
--- app/admin/image_map_polygon.rb
+++ app/admin/image_map_polygon.rb
@@ -1,3 +1,16 @@
 ActiveAdmin.register ImageMapPolygon do
   permit_params :coordinates, :image_map_id, :electoral_race_id
+
+  index do
+    selectable_column
+    id_column
+    column :coordinates do |image_map_polygon|
+      truncate image_map_polygon.coordinates
+    end
+    column :image_map
+    column :electoral_race
+    column :created_at
+    column :updated_at
+    default_actions
+  end
 end
