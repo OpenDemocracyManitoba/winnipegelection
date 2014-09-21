eed1ae8ac19168374e2dbd9b690b0c2e827dbed8
Kyle Geske <stungeye@gmail.com>
2014-09-21 08:56:00 -0400

    Added the display of official doc link and past results tables to electoral races.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,9 @@ module ApplicationHelper
     @redcarpet ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML)
     @redcarpet.render(marked_down_content).html_safe
   end
+
+  def markdown_with_tables(marked_down_content)
+    @redcarpet ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, tables: true)
+    @redcarpet.render(marked_down_content).html_safe
+  end
 end
