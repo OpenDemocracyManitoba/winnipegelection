d9c270cc017ffd2c6eb9147395298477a9e190a4
Rob Ring <rjm.ring@gmail.com>
2014-10-05 20:31:20 -0500

    Added additional scenarios to user_visits_election_issue_page spec

--- spec/features/user_visits_election_issue_page_spec.rb
+++ spec/features/user_visits_election_issue_page_spec.rb
@@ -12,6 +12,18 @@ feature 'User visits election issue page' do
         expect(page).to have_css('a', text: 'Election Issues')
       end
     end
-  end
 
+    scenario 'they are able to click on the Election Issues link to see the Election Issues Websites page' do
+      find(:css, "a:contains('Election Issues')").click
+      expect(page.status_code).to be(200)
+      expect(page).to have_css('h1', text: 'Election Issue Websites')
+    end
+
+    scenario 'they are able to see issue website listings for the current election when they visit the election issue page' do
+      find(:css, "a:contains('Election Issues')").click
+      within('ul#issue_websites') do
+        expect(page).to have_css('a', count: 1)
+      end
+    end
+  end
 end
